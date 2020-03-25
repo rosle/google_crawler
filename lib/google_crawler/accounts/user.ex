18ca63c804e9c5ad930e3afe2eeaef568965b660
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-25 20:08:18 +0700

    Change the lib to bcrypt and implement the test

--- lib/google_crawler/accounts/user.ex
+++ lib/google_crawler/accounts/user.ex
@@ -13,13 +13,23 @@ defmodule GoogleCrawler.Accounts.User do
     timestamps()
   end
 
+  @fields ~w(email username encrypted_password password password_confirmation)a
+
+  # TODO: Add Password confirmation
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :username, :encrypted_password, :password, :password_confirmation])
-    |> validate_required([:email, :username, :password, :password_confirmation])
+    |> cast(attrs, @fields)
+    |> validate_required([:email, :username])
     |> validate_format(:email, ~r/@/)
-    |> validate_confirmation(:password)
+    |> validate_length(:password, min: 6)
     |> unique_constraint(:email)
     |> unique_constraint(:username)
+    |> put_pass_hash
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :encrypted_password, Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
 end
