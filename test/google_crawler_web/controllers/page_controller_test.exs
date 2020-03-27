4a2531b94f30834e22c03cf5704d7854f88fc3e9
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-27 16:20:00 +0700

    Add test for protected routes

--- test/google_crawler_web/controllers/page_controller_test.exs
+++ test/google_crawler_web/controllers/page_controller_test.exs
@@ -1,8 +1,27 @@
 defmodule GoogleCrawlerWeb.PageControllerTest do
   use GoogleCrawlerWeb.ConnCase
+  use Plug.Test
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+  alias GoogleCrawler.UserFactory
+
+  test "GET / renders the index template", %{conn: conn} do
+    user = UserFactory.create()
+
+    conn =
+      conn
+      |> init_test_session(%{})
+      |> put_session(:current_user_id, user.id)
+      |> get(Routes.page_path(conn, :index))
+
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+
+  test "GET / redirects to the login page if the user has not logged in", %{conn: conn} do
+    conn =
+      conn
+      |> init_test_session(%{})
+      |> get(Routes.page_path(conn, :index))
+
+    assert redirected_to(conn) == Routes.session_path(conn, :new)
   end
 end
