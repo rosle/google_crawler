cdceab640025ba608e92da6f1a1224c403f3cbde
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-30 09:39:26 +0700

    Remove list, update and delete user functions

--- lib/google_crawler/accounts.ex
+++ lib/google_crawler/accounts.ex
@@ -9,19 +9,6 @@ defmodule GoogleCrawler.Accounts do
   alias GoogleCrawler.Accounts.User
 
   @doc """
-  Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
-  """
-  def list_users do
-    Repo.all(User)
-  end
-
-  @doc """
   Gets a single user.
 
   Raises `Ecto.NoResultsError` if the User does not exist.
@@ -53,52 +40,5 @@ defmodule GoogleCrawler.Accounts do
     %User{}
     |> User.registration_changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_user(%User{} = user, attrs) do
-    user
-    |> User.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a user.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_user(%User{} = user) do
-    Repo.delete(user)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{source: %User{}}
-
-  """
-  def change_user(%User{} = user) do
-    User.changeset(user, %{})
   end
 end
--- test/google_crawler/accounts_test.exs
+++ test/google_crawler/accounts_test.exs
@@ -7,17 +7,6 @@ defmodule GoogleCrawler.AccountsTest do
   describe "users" do
     alias GoogleCrawler.Accounts.User
 
-    test "list_users/0 returns all users" do
-      user = UserFactory.create()
-
-      user_id =
-        Accounts.list_users()
-        |> List.first()
-        |> Map.get(:id)
-
-      assert user_id == user.id
-    end
-
     test "get_user!/1 returns the user with given id" do
       user = UserFactory.create()
 
@@ -37,36 +26,6 @@ defmodule GoogleCrawler.AccountsTest do
       user_attrs = UserFactory.build_attrs(%{email: nil, username: nil, password: nil})
 
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(user_attrs)
-    end
-
-    test "update_user/2 with valid data updates the user" do
-      user = UserFactory.create()
-      new_user_attrs = UserFactory.build_attrs()
-
-      assert {:ok, %User{} = user} = Accounts.update_user(user, new_user_attrs)
-      assert user.email == new_user_attrs.email
-      assert user.username == new_user_attrs.username
-    end
-
-    test "update_user/2 with invalid data returns error changeset" do
-      user = UserFactory.create()
-      new_user_attrs = UserFactory.build_attrs(%{email: nil, username: nil, password: nil})
-
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, new_user_attrs)
-      assert user.id == Accounts.get_user!(user.id).id
-    end
-
-    test "delete_user/1 deletes the user" do
-      user = UserFactory.create()
-
-      assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
-    end
-
-    test "change_user/1 returns a user changeset" do
-      user = UserFactory.create()
-
-      assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
 end
