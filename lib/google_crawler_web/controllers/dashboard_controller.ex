a91b0b5afefd9591517a9a3d9ac12fffc8351a3d
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-31 09:43:51 +0700

    Display keyword list

--- lib/google_crawler_web/controllers/dashboard_controller.ex
+++ lib/google_crawler_web/controllers/dashboard_controller.ex
@@ -1,11 +1,13 @@
 defmodule GoogleCrawlerWeb.DashboardController do
   use GoogleCrawlerWeb, :controller
 
+  alias GoogleCrawler.Search
   alias GoogleCrawler.Search.KeywordFile
 
   def index(conn, _params) do
+    keywords = Search.list_keywords()
     changeset = KeywordFile.changeset(%KeywordFile{})
 
-    render(conn, "index.html", changeset: changeset)
+    render(conn, "index.html", keywords: keywords, changeset: changeset)
   end
 end
