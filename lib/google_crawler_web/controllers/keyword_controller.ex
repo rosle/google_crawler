4f85743fee9a4e942e318211518a7c395097e230
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-04-01 15:02:54 +0700

    Add keyword show page

---
+++ lib/google_crawler_web/controllers/keyword_controller.ex
@@ -0,0 +1,11 @@
+defmodule GoogleCrawlerWeb.KeywordController do
+  use GoogleCrawlerWeb, :controller
+
+  alias GoogleCrawler.Search
+
+  def show(conn, %{"id" => id}) do
+    keyword = Search.get_keyword(id)
+
+    render conn, "show.html", keyword: keyword
+  end
+end
