98c3e6c771aa6f86ac5aad9a14dc802b0504dc14
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-04-03 10:36:39 +0700

    Fix typo

--- test/google_crawler/search/search_keyword_task_test.exs
+++ test/google_crawler/search/search_keyword_task_test.exs
@@ -4,7 +4,7 @@ defmodule GoogleCrawler.Search.SearchKeywordTaskTest do
   alias GoogleCrawler.Search.Keyword
   alias GoogleCrawler.Search.SearchKeywordTask
 
-  test "perform/1 returns the scapping result when it is success" do
+  test "perform/1 returns the scrapping result when it is success" do
     assert %{raw_html_result: result} = SearchKeywordTask.perform(%Keyword{keyword: "elixir"})
   end
 
