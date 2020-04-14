d78b5a8ab75cc89a7bf6873b2c2c9693c5248905
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-04-14 15:49:05 +0700

    Scrap the links from google result

--- lib/google_crawler/google/scrapper.ex
+++ lib/google_crawler/google/scrapper.ex
@@ -1,14 +1,23 @@
 defmodule GoogleCrawler.Google.ScrapperResult do
   defstruct raw_html_result: nil,
-            total_results: 0
+            total_results: 0,
+            links: [],
+            total_links: 0,
+            top_ads_links: [],
+            total_top_ads_links: 0,
+            bottom_ads_links: [],
+            total_bottom_ads_links: 0
 end
 
 defmodule GoogleCrawler.Google.Scrapper do
   alias GoogleCrawler.Google.ScrapperResult
 
-  # @selectors %{
-  #   total_result: "#result-stats"
-  # }
+  @selectors %{
+    total_result: "#result-stats",
+    non_ads_links: "div.r > a",
+    top_ads_links: "#tads .ads-ad > .ad_cclk > a.V0MxL",
+    bottom_ads_links: "#bottomads .ads-ad > .ad_cclk > a.V0MxL"
+  }
 
   def scrap(html) do
     result = %ScrapperResult{}
@@ -17,26 +26,61 @@ defmodule GoogleCrawler.Google.Scrapper do
 
     parse_raw_html_result(result, html)
     |> parse_total_results(document)
+    |> parse_non_ads_links(document)
+    |> parse_top_ads_links(document)
+    |> parse_bottom_ads_links(document)
   end
 
-  def parse_total_results(result, document) do
-    total_results_text = Floki.find(document, "#result-stats") |> Floki.text
+  # TODO: Make sure the result page is in English to match the regex
+  defp parse_total_results(result, document) do
+    total_results_text = Floki.find(document, "#result-stats") |> Floki.text()
 
     total_results =
       Regex.named_captures(~r/About (?<total_result>.*) results/, total_results_text)
       |> Map.get("total_result")
       |> String.replace(",", "")
-      |> Integer.parse
+      |> Integer.parse()
       |> elem(0)
 
-    %{ result | total_results: total_results }
+    %{result | total_results: total_results}
   end
 
-  def parse_raw_html_result(result, html) do
-    %{ result | raw_html_result: cleanup_html(html) }
+  defp parse_non_ads_links(result, document) do
+    non_ads_links =
+      document
+      |> Floki.find(@selectors.non_ads_links)
+      |> Floki.attribute("href")
+
+    %{result | links: non_ads_links, total_links: length(non_ads_links)}
   end
 
-  def cleanup_html(html) do
+  defp parse_top_ads_links(result, document) do
+    top_ads_links =
+      document
+      |> Floki.find(@selectors.top_ads_links)
+      |> Floki.attribute("href")
+
+    %{result | top_ads_links: top_ads_links, total_top_ads_links: length(top_ads_links)}
+  end
+
+  defp parse_bottom_ads_links(result, document) do
+    bottom_ads_links =
+      document
+      |> Floki.find(@selectors.bottom_ads_links)
+      |> Floki.attribute("href")
+
+    %{
+      result
+      | bottom_ads_links: bottom_ads_links,
+        total_bottom_ads_links: length(bottom_ads_links)
+    }
+  end
+
+  defp parse_raw_html_result(result, html) do
+    %{result | raw_html_result: cleanup_html(html)}
+  end
+
+  defp cleanup_html(html) do
     html
     |> String.chunk(:printable)
     |> Enum.filter(&String.printable?/1)
--- test/google_crawler/google/scrapper_test.exs
+++ test/google_crawler/google/scrapper_test.exs
@@ -8,15 +8,41 @@ defmodule GoogleCrawler.Google.ScrapperTest do
     html = response_fixtures("hotels.html")
 
     result = Scrapper.scrap(html)
+    raw_html = cleanup_html(html)
 
-    IO.inspect result
-    # assert %ScrapperResult{
-    #          raw_html_result: html
-    #        } == result
+    assert %ScrapperResult{
+             raw_html_result: ^raw_html,
+             total_results: 5_970_000_000,
+             links: [
+               "https://www.hotels.com/",
+               "https://www.booking.com/city/th/bangkok.html",
+               "https://www.tripadvisor.com/Hotels-g293916-Bangkok-Hotels.html",
+               "https://www.agoda.com/city/bangkok-th.html",
+               "https://www.agoda.com/country/thailand.html",
+               "https://www.expedia.co.th/en/Bangkok-Hotels.d178236.Travel-Guide-Hotels",
+               "http://www.bangkok.com/hotels/",
+               "https://www.trivago.com/bangkok-519/hotel",
+               "https://www.hotelscombined.com/Place/Bangkok.htm"
+             ],
+             total_links: 9,
+             top_ads_links: [
+               "https://www.booking.com/"
+             ],
+             total_top_ads_links: 1,
+             bottom_ads_links: [],
+             total_bottom_ads_links: 0
+           } = result
   end
 
   defp response_fixtures(path) do
     Path.join(["test/fixtures/search_results", path])
     |> File.read!()
+  end
+
+  defp cleanup_html(html) do
+    html
+    |> String.chunk(:printable)
+    |> Enum.filter(&String.printable?/1)
+    |> Enum.join()
   end
 end
